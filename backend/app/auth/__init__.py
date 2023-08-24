0639637c317885189b7bbadbb7c72ef9d4d363bb
Jacob Immich <jacobimmich@gmail.com>
2023-08-24 02:21:22 -0400

    Fix imports

--- backend/app/auth/__init__.py
+++ backend/app/auth/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
-from . import controller, schema  # noqa
+from . import controller  # noqa
--- backend/app/oauth/__init__.py
+++ backend/app/oauth/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 oauth = Blueprint('oauth', __name__)
 
-from . import controller, schema  # noqa
+from . import controller  # noqa
