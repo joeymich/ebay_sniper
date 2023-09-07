dddafc4918c5f4230494d044725e3f66140a7237
Jacob Immich <jacobimmich@gmail.com>
2023-09-07 13:42:48 -0400

    Add useAuth hook

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -3,5 +3,14 @@ import { AuthContext } from '../context/AuthContext';
 
 export const useAuth = () => {
     const { user, setUser } = useContext(AuthContext);
-    return { user, setUser };
+
+    const isEmailVerified = (): boolean => {
+        return user?.email_verified ? true : false;
+    }
+
+    const isEbayConnected = (): boolean => {
+        return user?.ebay_user_id ? true : false;
+    }
+
+    return { user, setUser, isEmailVerified, isEbayConnected };
 }
